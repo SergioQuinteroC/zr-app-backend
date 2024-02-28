01feace55353fc99072fc8d3330227c2db21ad50
unknown <sergio.quinteroc@udea.edu.co>
2024-02-28 16:45:45 -0500

    add routing to realestate

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("my apps");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Hello world");
+});
+
+app.get("/realestate", (req, res) => {
+	res.json({
+		id: 1,
+		title: "Lote tal",
+		description: "blablabal",
+		price: 100,
+		images: ["image1", "image2"],
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port http://localhost:${port}`);
+});
